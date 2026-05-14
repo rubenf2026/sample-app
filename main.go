74b5f2763ed64cddcbfe10b16900a568ecdd1b65
rubenf2026 <rubenferreira1197@gmail.com>
2026-05-14 11:12:30 +0000

    test

--- main.go
+++ main.go
@@ -1,4 +1,3 @@
-/**
  * Copyright 2023 Google Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -17,29 +16,20 @@
 package main
 
 import (
-	"image"
-	"image/color"
-	"image/draw"
-	"image/png"
-	"net/http"
+        "image"
+        "image/color"
+        "image/draw"
+        "image/png"
+        "net/http"
 )
 
 func main() {
-	http.HandleFunc("/blue", blueHandler)
-	http.HandleFunc("/red", redHandler)
-	http.ListenAndServe(":8080", nil)
+        http.HandleFunc("/blue", blueHandler)
+        http.ListenAndServe(":8080", nil)
 }
 
 func blueHandler(w http.ResponseWriter, r *http.Request) {
-	img := image.NewRGBA(image.Rect(0, 0, 100, 100))
-	draw.Draw(img, img.Bounds(), &image.Uniform{color.RGBA{0, 0, 255, 255}}, image.ZP, draw.Src)
-	w.Header().Set("Content-Type", "image/png")
-	png.Encode(w, img)
-}
-
-func redHandler(w http.ResponseWriter, r *http.Request) {
-	img := image.NewRGBA(image.Rect(0, 0, 100, 100))
-	draw.Draw(img, img.Bounds(), &image.Uniform{color.RGBA{255, 0, 0, 255}}, image.ZP, draw.Src)
-	w.Header().Set("Content-Type", "image/png")
-	png.Encode(w, img)
-}
+        img := image.NewRGBA(image.Rect(0, 0, 100, 100))
+        draw.Draw(img, img.Bounds(), &image.Uniform{color.RGBA{0, 0, 255, 255}}, image.ZP, draw.Src)
+        w.Header().Set("Content-Type", "image/png")
+        png.Encode(w, img)
